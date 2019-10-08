6df6b189ea2850d37ec0353c8736c90a6c9845ee
Mavis-goz <yasinsevik13@gmail.com>
2019-10-08 12:03:46 +0200

    rage

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert(0);
+let gasten = 42;
+let deelExtraPorties = 3;
+let extraPorties = gasten / deelExtraPorties;
+let totaalPorties  = gasten + extraPorties;
+
+let mlPerKom = 250;
+let mlPerPan = 4000;
+
+let totaalAantalKommen = totaalPorties * mlPerKom;
+
+let result = Math.ceil(totaalAantalKommen / mlPerPan);
+
+document.getElementById("result").innerHTML += result;
